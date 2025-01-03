e14fd3efa52df7567395099aae0a9443fa634299
Patrick-864 <p.schneider@student.vu.nl>
2025-01-04 00:20:03 +0100

    It still workssss

--- Hello.cpp
+++ Hello.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "Hello World!";
     std::cout << "Sth changed";
-    std::cout << "Maybe I know how it works?";
+    std::cout << "Maybe I know how it wo?";
     return 0;
 }  
